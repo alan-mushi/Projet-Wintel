97130a11e35336a058f21988ed708c8e5753177c
zipang29 <guillaume.claudic@laposte.net>
2012-05-10 18:27:27 +0200

    Ajout de l'ecouteur pour l'action quitter

--- src/ihm/EcouteurItemQuitter.java
+++ src/ihm/EcouteurItemQuitter.java
@@ -3,14 +3,14 @@ package ihm;
 import javax.swing.*;
 import java.awt.event.*;
 
-class EcouteurItemCharger implements ActionListener {
+class EcouteurItemQuitter implements ActionListener {
 	private Wintel theWintel;
 	
-	public EcouteurItemCharger(Wintel unWintel) {
+	public EcouteurItemQuitter(Wintel unWintel) {
 		this.theWintel = unWintel;
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		theWintel.chargerEtAfficherAnnuaire();
+		theWintel.dispose(); 
 	}
 }
