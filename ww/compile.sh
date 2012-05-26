e2ed75c846d7bcc41634f62c1f4677d0a22a7361
Thibault GUITTET <alan_mushi@yahoo.com>
2012-05-26 20:27:33 +0200

    Pour plus de confort...

--- ww/compile.sh
+++ ww/compile.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-#Script permettant de compiler le projet d'AP4 Wintel et de l'exécuter tout de suite
+# Script permettant de compiler le projet d'AP4 Wintel et de l'exécuter tout de suite
 
 javac -encoding UTF8 -d ../class ../src/ihm/*.java ../src/datas/*.java ../src/control/*.java
-echo "Exécuter Wintel ? (o/n : defaut) : "
+echo -n "Exécuter Wintel ? (o/n : defaut) : "
 read ok
 
 if [ "$ok" = "o" ] || [ "$ok" = "O" ]; then
+	if [ $CLASSPATH != "../class" ] ; then
+		export CLASSPATH=../class
+	fi
 	java ihm.Wintel
 fi
 
