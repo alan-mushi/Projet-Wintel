f098a18d3f3d0d99afd31e445c72a63c5e196f46
Thibault GUITTET <alan_mushi@yahoo.com>
2012-05-31 13:02:28 +0200

    Ajout d'une condition pour la machine de guillaume

--- javadoc.sh
+++ javadoc.sh
@@ -5,8 +5,13 @@ machine=`uname -n`
 
 if [ $machine = "bt" ] || [ $machine = "thibault-laptop" ]; then
 	cd ~/IUT/AP4/Projet-Wintel/
+	
+elif [ $machine = "Skynet" ] ; then
+	cd C:/Users/Guillaume/Desktop/Projet-Wintel/
+	
 else
 	echo Votre path ici
+	exit
 fi
 
 javadoc -docencoding utf8 -private -d doc/ src/datas/* src/ihm/* src/control/*
